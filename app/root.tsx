d2a84809c0be930c646ed63b95c490d9fdff731c
hctaw-srp <scarsdaleny15@yahoo.co.jp>
2024-09-05 21:49:44 +0900

    ~fix speed insights

--- app/root.tsx
+++ app/root.tsx
@@ -4,31 +4,33 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+} from '@remix-run/react'
+import { SpeedInsights } from '@vercel/speed-insights/remix'
+import './tailwind.css'
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
       <body>
         {children}
+        <SpeedInsights />
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
 }
 
 export function HydrateFallback() {
-  return <p>Loading...</p>;
+  return <p>Loading...</p>
 }
