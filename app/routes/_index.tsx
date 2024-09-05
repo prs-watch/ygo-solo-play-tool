e40179b282f5517602304df877c119c7dc50896b
hctaw-srp <scarsdaleny15@yahoo.co.jp>
2024-09-05 20:20:40 +0900

    ~fix lighthouse suggestion

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,7 +13,9 @@ import { v4 as uuidv4 } from 'uuid'
  * @returns - メタファンクション
  */
 export const meta: MetaFunction = () => {
-  return [{ title: 'YGO Solo-Play Tool' }]
+  return [
+    { title: 'YGO Solo-Play Tool', description: 'Tool to play YGO as solo.' },
+  ]
 }
 
 /**
