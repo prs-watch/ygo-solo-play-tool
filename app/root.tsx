f08a73a5b9d001b8dabde9a2bfbabdb934f20bff
hctaw-srp <scarsdaleny15@yahoo.co.jp>
2024-09-09 20:34:34 +0900

    ~fix meta tag

--- app/root.tsx
+++ app/root.tsx
@@ -15,8 +15,28 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta property='og:title' content='YGO Solo Play Tool' />
+        <meta
+          property='og:description'
+          content='A tool for solo playing Yu-Gi-Oh!'
+        />
+        <meta property='og:image' content='/og-image.png' />
+        <meta
+          property='og:url'
+          content='https://ygo-solo-play-tool.vercel.app'
+        />
+        <meta property='og:type' content='website' />
         <meta name='twitter:card' content='summary' />
-        <meta property="og:image" content="/og-image.png" />
+        <meta name='twitter:title' content='YGO Solo Play Tool' />
+        <meta
+          name='twitter:description'
+          content='A tool for solo playing Yu-Gi-Oh!'
+        />
+        <meta name='twitter:image' content='/og-image.png' />
+        <meta
+          name='twitter:url'
+          content='https://ygo-solo-play-tool.vercel.app'
+        />
         <Meta />
         <Links />
       </head>
