eb8afb64e299be00da9b28531fc41736bf997246
hctaw-srp <scarsdaleny15@yahoo.co.jp>
2024-09-09 19:59:06 +0900

    ~fix og card

--- app/root.tsx
+++ app/root.tsx
@@ -15,6 +15,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='twitter:card' content='summary' />
+        <meta property="og:image" content="og-image.png" />
         <Meta />
         <Links />
       </head>
